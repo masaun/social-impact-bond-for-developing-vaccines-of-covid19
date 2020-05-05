6a95061d6d50668ddcfef5b5ca9cbd7209757934
masaun <masanoriuno@gmail.com>
2020-05-05 13:57:00 +0900

    Update contractAddress.js in ./client/src/data

--- migrations/contractAddress/contractAddress.js
+++ migrations/contractAddress/contractAddress.js
@@ -4,7 +4,7 @@ module.exports = {
             "": "",          
             "": "", 
             "": ""
-        }
+        },
         "BokkyPooBahsDateTimeLibrary": {
             "BokkyPooBahsDateTimeContract": "0x2ac864D31b541267A821408A952E954B6b2AC9eA"
         }
