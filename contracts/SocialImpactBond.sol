3989193743e08e8451126f3b484304569e45e633
masaun <masanoriuno@gmail.com>
2020-05-08 09:03:11 +0900

    Using dai.balanceOf() instead of address(contract).balance

--- contracts/SocialImpactBond.sol
+++ contracts/SocialImpactBond.sol
@@ -167,7 +167,8 @@ contract SocialImpactBond is OwnableOriginal(msg.sender), McStorage, McConstants
         bool _isAchieved = objective.isAchieved;
 
         uint _countTargetInvestors = countTargetInvestors(_objectiveId);
-        uint balanceOfObjectiveId = address(objective.objectiveAddress).balance;
+        uint balanceOfObjectiveId = dai.balanceOf(address(objective.objectiveAddress));
+        //uint balanceOfObjectiveId = address(objective.objectiveAddress).balance;
         uint dividedAmount = balanceOfObjectiveId.div(_countTargetInvestors);
 
         //@dev - Only investors who invested for achived objective can receive returned money (principal amounts plus interest amounts)
